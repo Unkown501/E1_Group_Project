bedd99c62d16cb41112a2615db7dee8753101443
HuerTz0 <shouvikdev07@gmail.com>
2026-02-12 17:03:44 -0800

    Added dummy sprites and movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    float movementX;
+    float movementY;
+    [SerializeField] float speed = 2.0f;
+
+    void OnMove(InputValue value)
+    {
+        Vector2 movementVector = value.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void FixedUpdate()
+    {
+        float XmoveDistance = movementX * speed * Time.fixedDeltaTime;
+        float YmoveDistance = movementY * speed * Time.fixedDeltaTime;
+        transform.position = new Vector2(transform.position.x + XmoveDistance, transform.position.y + YmoveDistance);
+    }
     void Start()
     {
         
