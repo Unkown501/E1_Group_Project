ce903238e702ff16c8b22b63563356cdd0f2ede0
HuerTz0 <shouvikdev07@gmail.com>
2026-02-19 16:48:24 -0800

    Doorway Attempt

---
+++ Assets/Scripts/DoorwayScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DoorwayScript : MonoBehaviour
+{
+    [SerializeField] NewSceneLoader sceneLoader;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+             sceneLoader.LoadScene();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
