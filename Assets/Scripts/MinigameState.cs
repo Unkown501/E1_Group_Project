97f3e9099ce8c665678e2f621e4f2121004d626a
Raymond Zou <raymond.yh.zou@gmail.com>
2026-03-11 01:16:36 -0700

    Update MinigameState.cs

--- Assets/Scripts/MinigameState.cs
+++ Assets/Scripts/MinigameState.cs
@@ -1,9 +1,16 @@
-using UnityEngine;
+using System.Collections.Generic;
 
 public static class MinigameState
 {
     public static string InitialState = "";
     public static string ReturnScene = "";
     public static Vector2 ReturnPosition;
+
+    public static Dictionary<string, bool> CompletionStatus  = new Dictionary<string, int>()
+    {
+        { "Terminal1", false },
+        { "Terminal2", false }
+    };
+
 
 }
