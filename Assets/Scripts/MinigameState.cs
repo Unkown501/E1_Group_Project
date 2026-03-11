3d5fe2a3f32e3b5c8722b1c26b1c4fe243aacb6d
Raymond Zou <raymond.yh.zou@gmail.com>
2026-03-11 01:18:36 -0700

    another fix

--- Assets/Scripts/MinigameState.cs
+++ Assets/Scripts/MinigameState.cs
@@ -7,7 +7,7 @@ public static class MinigameState
     public static string ReturnScene = "";
     public static Vector2 ReturnPosition;
 
-    public static Dictionary<string, bool> CompletionStatus  = new Dictionary<string, int>()
+    public static Dictionary<string, bool> CompletionStatus  = new Dictionary<string, bool>()
     {
         { "Terminal1", false },
         { "Terminal2", false }
